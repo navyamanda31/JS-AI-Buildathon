6ca473930b049e64a839e614ab3a244ca4a3784c
navyamanda3106 <navya.btech2024@spsu.ac.in>
2025-06-13 02:23:59 +0530

    Connect to Azure AI Foundry

---
+++ ai-foundry.js
@@ -0,0 +1,25 @@
+import ModelClient from "@azure-rest/ai-inference";
+import { AzureKeyCredential } from "@azure/core-auth";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+  const client = new ModelClient(
+  process.env.AZURE_INFERENCE_SDK_ENDPOINT ?? "https://navya-mbtu5ci0-swedencentral.services.ai.azure.com/models", new AzureKeyCredential(process.env.AZURE_INFERENCE_SDK_KEY ?? "7sfuHECW8eApUosbOCOeflm0KAslBEtwWP0CccT22GtsMzwHvixQJQQJ99BFACfhMk5XJ3w3AAAAACOG491r"));
+
+var messages = [
+  { role: "system", content: "You are an helpful assistant" },
+  { role: "user", content: "What are 3 things to see in Seattle?" },
+];
+
+var response = await client.path("chat/completions").post({
+  body: {
+    messages: messages,
+    max_tokens: 4096,
+      temperature: 1,
+      top_p: 1,
+      model: "gpt-4.1",
+  },
+});
+
+console.log(JSON.stringify(response));
